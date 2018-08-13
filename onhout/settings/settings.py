6e711eebbdcadc144e087daf1dca76d2684c7853
Peter Liu <onhout@gmail.com>
2018-08-13 12:41:29 -0700

    procfile added

--- onhout/settings/settings.py
+++ onhout/settings/settings.py
@@ -188,4 +188,3 @@ WAGTAIL_SITE_NAME = "onhout"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://www.onhout.com'
-DISABLE_COLLECTSTATIC=1 # because of heroku
